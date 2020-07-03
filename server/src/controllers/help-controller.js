e4a382cf5da12e8a12459f27023202221b19a2d8
leoaltemari <leo.altemari@usp.br>
2020-07-03 03:09:22 -0300

    Creating a get route to the 'help'.

--- server/src/controllers/help-controller.js
+++ server/src/controllers/help-controller.js
@@ -4,13 +4,21 @@ const validateHelp = new Validator();
 const PostHelp = require('../models/Help');
 
 exports.post = async (req, res, next) => {
-    validateHelp.validateData(req.body);
+    // Validacao de dados
+    try {
+        validateHelp.validateData(req.body);
+    } catch(err) {
+        res.status(400).json( {message: err });
+    }
+
+    // Crianddo objeto de acordo com o modelo passado(PostHelp)
     const post = new PostHelp({
         name: req.body.name,
         phoneNumber: req.body.phoneNumber,
         state: req.body.state
     });
 
+    // Salvando no Banco de Dados
     try {
         const saved = await post.save();
         res.status(200).json(saved);
@@ -21,5 +29,10 @@ exports.post = async (req, res, next) => {
 };
 
 exports.get = async (req, res,  next) => {
-    // todo: implement the get accordding to the database
+    try {
+        const allHelpPosts = await PostHelp.find();
+        res.status(200).json(allHelpPosts);
+    } catch(err) {
+        res.status(400).json( {message: err });
+    }
 };
